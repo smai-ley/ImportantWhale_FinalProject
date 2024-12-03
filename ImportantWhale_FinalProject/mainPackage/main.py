a280a854702d72e41ba38359b4579bcb4fdd8b06
smai-ley <tkrkh43@gmail.com>
2024-12-03 08:12:52 -0500

    Add project files.

---
+++ ImportantWhale_FinalProject/mainPackage/main.py
@@ -0,0 +1,13 @@
+# Name: Sarah Mahan, Denise Huynh, Quynh Doan, Roman Stryjewski
+# email:  mahansa@mail.uc.edu, huynhd2@mail.uc.edu, doanqb@mail.uc.edu, stryjerj@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 12/10/24 7:00AM
+# Course #/Section:   IS4010 001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment:  
+
+# Brief Description of what this module does:
+# Citations:
+# Anything else that's relevant:
+
+# main.py
